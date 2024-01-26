dc58eeff605e1acb3a7b26e5d3ee4bb1eec88ad1
testuser3 <testuser3@doubleslash.de>
2024-01-26 13:53:42 +0100

    Build pipeline WIP

--- wait-for-service.sh
+++ wait-for-service.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/api-docs); do
+
+#until $(curl --output /dev/null --silent --head --fail http://localhost:8080/api-docs); do
+until $(curl --output /dev/null --head --fail http://localhost:8080/api-docs); do
     printf '.'
     sleep 5
 done
